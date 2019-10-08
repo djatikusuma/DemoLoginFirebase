03bb5034d8a6ca52666e3d9d62b3da0979f0b2a8
DjatikusumaID <djatikusumadata@gmail.com>
2019-10-08 09:28:58 +0700

    Initial Commit

---
+++ DemoLoginFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoLoginFirebase
+//
+//  Created by Rangga Djatikusuma Lukman on 08/10/19.
+//  Copyright © 2019 Codekinian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
